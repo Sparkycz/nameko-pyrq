85031076eaca2d043d68a43461a2587f0b0533e9
Vladimír Kašpar <vladimir.kaspar@heureka.cz>
2017-06-26 14:30:16 +0200

    bump version to 2.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='nameko-pyrq',
-    version='1.1.0',
+    version='2.0.0',
     url='https://github.com/Sparkycz/nameko-pyrq/tree/master',
     license='The MIT License',
     author='Sparkycz',
